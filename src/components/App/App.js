0748323c8bb83f565bd097481cc97d21369b3670
randyc92 <costarandy92@gmail.com>
2024-05-17 23:08:23 -0400

    Remove Tracks from a Playlist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
       ]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track){
@@ -31,6 +32,13 @@ class App extends React.Component {
     this.setState( {playlistTracks: tracks} )
   }
 
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState( {playlistTracks: tracks} );
+  }
+
   render() {
     return (
       <div>
@@ -39,7 +47,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -7,7 +7,7 @@ class Playlist extends React.Component {
         return (
             <div className="Playlist">
                 <input defaultValue={"New Playlist"}/>
-                <TrackList tracks={this.props.playlistTracks} name={this.props.playlistName} />
+                <TrackList tracks={this.props.playlistTracks} name={this.props.playlistName} onRemove={this.props.onRemove} isRemoval={true} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
